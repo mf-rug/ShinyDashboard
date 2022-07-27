c2437aa5a1973b533552a502a33f82e0607b1614
mf-rug <max.fuerst@gmail.com>
2022-07-27 18:26:58 +0200

    mpi

--- ui.R
+++ ui.R
@@ -20,19 +20,44 @@ dashboardPage(
     )
   ),
   dashboardBody(
+    tags$head(
+      tags$style(HTML("
+      .frame {
+        width: 50vw;
+        border: 3px solid white;
+        background: #eee;
+        margin: auto;
+        padding: 15px 10px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    img {
+      width: 100%;
+      height: 100%;
+    }"))
+    ),
     tabItems(
       tabItem(tabName = "AA2Codon",
               h2("AA2Codon"),
-              HTML("<i><h4>A tiny shiny app that calculates possible codons given a selection of amino acids</h4></i>"),
-              tags$a(
-                href="AA2Cod", 
-                tags$img(src="/AA2Cod.png", 
-                         title="Example Image Link", 
-                         width="60%")
-              )
+              HTML("<i><h4>A tiny shiny app that calculates possible codons, given a selection of amino acids</h4></i>"),
+              div(class = 'frame', 
+                  tags$a(
+                  href="AA2Cod", 
+                  tags$img(src="/AA2Cod.png", 
+                           title="Access App", 
+                           width="60%")
+              ))
       ),
       tabItem(tabName = "MultiplePrimerEntry",
-              h2("MultiplePrimerEntry")
+              h2("MultiplePrimerEntry"),
+              HTML("<i><h4>A tiny shiny app that conveniently converts variable input of primer names and sequences to a copy and paste ready output for various vendor platforms.</h4></i>"),
+              div(class = 'frame', 
+                  tags$a(
+                    href="AA2Cod", 
+                    tags$img(src="/MPI.png", 
+                             title="Access App", 
+                             width="60%")
+                  ))
+      ),
       ),
       tabItem(tabName = "TweetRial",
               h2("TweetRial")

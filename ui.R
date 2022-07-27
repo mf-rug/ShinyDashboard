473e2c6fa133f78ebd4cb1dacd784da3e0290fec
mf-rug <max.fuerst@gmail.com>
2022-07-27 17:26:45 +0200

    initial rough outline

---
+++ ui.R
@@ -0,0 +1,53 @@
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(title = 'Fürstlab Shiny Apps'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Tiny Shiny Bio Tools", tabName = "Tiny Shiny Bio Tools", icon = icon("wrench"), startExpanded = TRUE, 
+               menuSubItem("AA2Codon", "AA2Codon"),
+               menuSubItem("MultiplePrimerEntry", "MultiplePrimerEntry")
+      ),
+      menuItem("Other", tabName = "Other", icon = icon("th"), startExpanded = TRUE,
+               menuSubItem("TweetRial", "TweetRial"),
+               menuSubItem("WordleBattle", "WordleBattle")
+      ),
+      menuItem("Group-internal", tabName = "Group-internal", icon = icon("key"), startExpanded = TRUE,
+               menuSubItem("AFAS", "AFAS"),
+               menuSubItem("AlphaFold", "AlphaFold"),
+               menuSubItem("GroupStack", "GroupStack")
+      )
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "AA2Codon",
+              h2("AA2Codon"),
+              fluidRow(
+                box("plot1"),
+                box(title = "Controls",
+                    sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      tabItem(tabName = "MultiplePrimerEntry",
+              h2("MultiplePrimerEntry")
+      ),
+      tabItem(tabName = "TweetRial",
+              h2("TweetRial")
+      ),
+      tabItem(tabName = "WordleBattle",
+              h2("WordleBattle")
+      ),
+      tabItem(tabName = "AFAS",
+              h2("AFAS")
+      ),
+      tabItem(tabName = "AlphaFold",
+              h2("AlphaFold")
+      ),
+      tabItem(tabName = "GroupStack",
+              h2("GroupStack")
+      )
+    )
+  )
+)
